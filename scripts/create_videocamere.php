57da66b5d2aacfa08dd309f9623d07309cff6ed0
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-03-31 13:58:05 +0200

    Cambio di colore

---
+++ scripts/create_videocamere.php
@@ -0,0 +1,9 @@
+<?php
+$url="https://www.dati.lombardia.it/resource/4ik7-d4js.json";
+$file=file_get_contents($url);
+$file=json_decode($file,true);
+foreach ($file as $singolo)
+{
+	echo $singolo['ztl']."</br>";
+}
+?>
