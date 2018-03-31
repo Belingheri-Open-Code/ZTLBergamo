c1115d5414ca0a97eff9b3710fa0037b8ad8eb4f
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-03-31 14:38:41 +0200

    Update Head
    salva in modo dinamico le pagine visitate uso di session

--- templates/head.php
+++ templates/head.php
@@ -1,45 +1,30 @@
 <head>
-	<!--pubblicità googole -->
 	<?php
-	/*
-	if ($_SESSION["dove"]=="contattami" || $_SESSION["dove"]=="descrizione"){
-		print("<script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>
-				<script>
-				  (adsbygoogle = window.adsbygoogle || []).push({
-					google_ad_client: \"ca-pub-7484192058927543\",
-					enable_page_level_ads: true
-				  });
-				</script>");
-	}else if($_SESSION["dove"]=="home")
-	{
-		echo "<link rel=\"canonical\" href=\"https://www.ztlbergamo.com/index.php\"/>"
-	}
-*/
+	$servername = "localhost";
+$username = "ztlbergamo";
+$password = "riccardo123";
+$dbname = "ztlbergamo";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    echo "<script>alert('problema interno');</script>";
+}
+$attData=date("Y-m-d");
+$sql = "UPDATE ultimoaccesso SET data='".$attData."' WHERE pagina='".$_SESSION["dove"]."'";
+if ($conn->query($sql) !== TRUE) {
+    echo "<script>alert('problema interno ".$conn->error."');</script>";
+} 
+$conn->close();
 	?>
-	<!--pubblicità googole -->
 	<title>ZTL Bergamo -Belingheri</title>
 	<meta charset="UTF-8">
 	<link rel="icon" href="/favicon.ico">
 	<META NAME="KEYWORDS" CONTENT="ztl,bergamo,zona a traffico limitato,traffico limitato ,ztl bergamo, zone a traffico limitato bergamo,zone traffico limitato bergamo">
 	<meta name="description" content="Stanco di prendere multe? Consulta ora in modo completamente gratuito le zone ZTL attive ORA a Bergamo! TUTTE le ZTL in Bergamo">
-	<!--
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-	<!--script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/jsg/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script-->
-	<!---->
 	<meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  	<!---->
-  	<!--link rel="stylesheet" href="https://getbootstrap.com/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script-->
 
 	<style>
 	p{
