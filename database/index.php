6b82dbddf957d000790976ee25cf96318b5e9679
Belingheri <33315640+Belingheri@users.noreply.github.com>
2018-05-24 11:10:39 +0200

    Update index.php

--- database/index.php
+++ database/index.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "ztlbergamo";
-$password = "riccardo123";
-$dbname = "ztlbergamo";
+$username = "";
+$password = "";
+$dbname = "";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
